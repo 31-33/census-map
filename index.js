280f6ccd99f4677ba102d7e81771ffcac6bcbcd4
Chris Symon <Chris.Symon97@gmail.com>
2019-05-02 14:10:47 +1200

    load map centered on NZ

---
+++ index.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-40.9006, 174.8860], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', 
+	{
+		foo: 'bar', 
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+	}).addTo(map);
