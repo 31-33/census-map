e5667290b36a992cc9b56568206d2a38127e3758
Chris Symon <Chris.Symon97@gmail.com>
2019-05-08 12:13:27 +1200

    fit map bounds to nz area

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-var map = L.map('map').setView([-40.9006, 174.8860], 6);
+var map = L.map('map');
+const nz_bounds = [[-33.87, 164.36], [-48.17, 179.30]];
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', 
 	{
 		foo: 'bar', 
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 	}).addTo(map);
+
+map.fitBounds(nz_bounds);
