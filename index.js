3c94b63fc37d3fdb11bd9e9201d2201433bd5ec5
Chris Symon <Chris.Symon97@gmail.com>
2019-05-09 14:07:15 +1200

    create polygon layer and draw polygons

--- index.js
+++ index.js
@@ -7,3 +7,41 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
 	}).addTo(map);
 
 map.fitBounds(nz_bounds);
+
+let layergroup = L.layerGroup().addTo(map);
+
+function drawPolygon(layergroup, coords, callback) {
+	const wkt = new Wkt.Wkt();
+	wkt.read(coords);
+	layergroup.addLayer(
+		wkt.toObject({
+			color: 'red',
+		}).on('click', () => callback(0))
+	);
+}
+
+function onRegionClicked(region_id) {
+	console.log(`User clicked region: ${region_id}`);
+	// TODO: fetch data for {region_id}
+
+	layergroup.clearLayers();
+	// TODO: fetch areas with {region_id} and draw on map
+}
+
+function onAreaClicked(area_id) {
+	console.log(`User clicked area: ${area_id}`);
+	// TODO: fetch data for {area_id}
+
+	layergroup.clearLayers();
+	// TODO: fetch meshblocks with {area_id} and draw on map
+}
+
+function onMeshblockClicked(meshblock_id) {
+	console.log(`User clicked meshblock: ${meshblock_id}`);
+
+	layergroup.clearLayers();
+	// TODO: fetch data for {meshblock_id}
+}
+
+let test_region = "MULTIPOLYGON (((168 -50, 170 -40, 176 -30)))";
+drawPolygon(layergroup, test_region, this.onRegionClicked);
