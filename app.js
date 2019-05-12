7d4407e16de0f6c2873c5b23c53933211ec7da2c
Chris Symon <chris.symon97@gmail.com>
2019-05-12 14:14:48 +1200

    implemented API endpoints to get coordinate data for regions/areas/meshblocks

--- app.js
+++ app.js
@@ -3,9 +3,36 @@ var sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3000;
 
+const db = new sqlite3.Database(`./data/census2013.db`, sqlite3.OPEN_READONLY, err => {
+    if(err) return console.log(`Error: ${err}`);
+    console.log('Initialized SQlite database');
+});
+
 var server = express();
 
 server.use('', express.static(`${__dirname}/client`));
 
+server.get('/regions', (req, res) => {
+    db.all(
+        "SELECT wkt, name, id FROM region_coordinates", 
+        (err, regions) => res.send(regions)
+    );
+});
+
+server.get('/areas/:region_id', (req, res) => {
+    db.all(
+        "SELECT wkt, name, id FROM area_coordinates WHERE region_id = ?",
+        req.params.region_id,
+        (err, areas) => res.send(areas)
+    );
+});
+
+server.get('/meshblocks/:area_id', (req, res) => {
+    db.all(
+        "SELECT wkt, name, id FROM meshblock_coordinates WHERE area_id = ?",
+        req.params.area_id,
+        (err, meshblocks) => res.send(meshblocks)
+    );
+});
 
 server.listen(PORT);
