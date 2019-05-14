1e8b72d172c8ff84ebda80cfe2775c7ed2957d0d
Chris Symon <chris.symon97@gmail.com>
2019-05-14 15:55:30 +1200

    zoom to focus selection

--- client/index.js
+++ client/index.js
@@ -9,45 +9,48 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
 map.fitBounds(nz_bounds);
 
 let layergroup = L.layerGroup().addTo(map);
+const wkt = new Wkt.Wkt();
 
-function drawPolygon(layergroup, coords, id, callback) {
-	const wkt = new Wkt.Wkt();
-	wkt.read(coords);
+function drawPolygon(layergroup, data, color, callback) {
+	wkt.read(data.wkt);
+	const polygon = wkt.toObject({
+		color: color,
+	});
 	layergroup.addLayer(
-		wkt.toObject({
-			color: 'red',
-		}).on('click', () => callback(id))
+		polygon.on('click', () => callback(data.id, polygon._bounds))
 	);
 }
 
-function onRegionClicked(region_id) {
+function onRegionClicked(region_id, bounds) {
 	console.log(`User clicked region: ${region_id}`);
 	// TODO: fetch data for {region_id}
 
+	map.fitBounds(bounds);
 	layergroup.clearLayers();
 	drawAreas(region_id);
 }
 
-function onAreaClicked(area_id) {
+function onAreaClicked(area_id, bounds) {
 	console.log(`User clicked area: ${area_id}`);
 	// TODO: fetch data for {area_id}
 
+	map.fitBounds(bounds);
 	layergroup.clearLayers();
 	drawMeshblocks(area_id);
 }
 
-function onMeshblockClicked(meshblock_id) {
+function onMeshblockClicked(meshblock_id, bounds) {
 	console.log(`User clicked meshblock: ${meshblock_id}`);
-
-	// layergroup.clearLayers();
 	// TODO: fetch data for {meshblock_id}
+
+	map.fitBounds(bounds);
 }
 
 function drawRegions(){
 	fetch('/regions')
 		.then(res => res.json())
 		.then(json => json.forEach(region => {
-			drawPolygon(layergroup, region.wkt, region.id, this.onRegionClicked);
+			drawPolygon(layergroup, region, 'red', this.onRegionClicked);
 		}));
 };
 
@@ -55,7 +58,7 @@ function drawAreas(region_id){
 	fetch(`/areas/${region_id}`)
 		.then(res => res.json())
 		.then(json => json.forEach(area => {
-			drawPolygon(layergroup, area.wkt, area.id, this.onAreaClicked);
+			drawPolygon(layergroup, area, 'yellow', this.onAreaClicked);
 		}));
 }
 
@@ -63,7 +66,7 @@ function drawMeshblocks(area_id){
 	fetch(`/meshblocks/${area_id}`)
 		.then(res => res.json())
 		.then(json => json.forEach(meshblock => {
-			drawPolygon(layergroup, meshblock.wkt, meshblock.id, this.onMeshblockClicked);
+			drawPolygon(layergroup, meshblock, 'green', this.onMeshblockClicked);
 		}));
 }
 
